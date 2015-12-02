ba8ce4b365431f9b6161d66aca92e51c094fdc18
James Childress <jchild@sas.upenn.edu>
2015-12-02 01:48:26 +0000

    Finish log in/log out

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,18 @@
 class UsersController < ApplicationController
-  def new
-    @user = User.new
-  end
-  
+
   def show
     @user = User.find(params[:id])
   end
-  
+
+  def new
+    @user = User.new
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
-      flash[:success] = "Welcome to the Workout Scheduler!"
+      log_in @user
+      flash[:success] = "Welcome to the Sample App!"
       redirect_to @user
     else
       render 'new'
@@ -23,5 +25,4 @@ class UsersController < ApplicationController
       params.require(:user).permit(:name, :email, :password,
                                    :password_confirmation)
     end
-  
 end
