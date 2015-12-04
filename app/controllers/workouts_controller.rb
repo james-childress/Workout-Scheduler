2168e78cd1a970e4a14b02ab33c280b3b1c77369
James Childress <jchild@sas.upenn.edu>
2015-12-04 20:53:48 +0000

    Added Edit and Delete buttons and pages for workouts

--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -17,6 +17,29 @@ class WorkoutsController < ApplicationController
     end
   end
   
+  def edit
+    @user = User.find(params[:user_id])
+    @workout = Workout.find(params[:id])
+  end
+  
+  def update
+    @user = User.find(params[:user_id])
+    @workout = Workout.find(params[:id])
+    if @workout.update_attributes(workout_params)
+      flash[:success] = "Workout updated"
+      redirect_to @user
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @user = User.find(params[:user_id])
+    Workout.find(params[:id]).destroy
+    flash[:success] = "Workout deleted"
+    redirect_to @user
+  end
+  
   private
 
     def workout_params
--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,5 +1,5 @@
 class Workout < ActiveRecord::Base
-  belongs_to :user, dependent: :destroy
+  belongs_to :user
   validates :program, presence: true
   validates :exercise, presence: true
   validates :training_max, presence: true
